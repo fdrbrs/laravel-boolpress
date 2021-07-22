cc6a4c2db8d04fd15d38d3801db77129b7a5c5eb
fdrbrs <federico.borsci@gmail.com>
2021-07-22 19:51:53 +0200

    create post model,controller, seeder, faker values

---
+++ database/seeds/PostSeeder.php
@@ -0,0 +1,23 @@
+<?php
+use Faker\Generator as Faker;
+use App\Post;
+use Illuminate\Database\Seeder;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i=0; $i < 11; $i++) { 
+            $post = new Post();
+            $post->title = $faker->sentence(3);
+            $post->body = $faker->text();
+            $post->image = $faker->imageUrl(600, 300, 'Post', true, $post->title);
+            $post->save();
+        }
+    }
+}
