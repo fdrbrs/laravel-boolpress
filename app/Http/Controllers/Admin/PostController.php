a63005cd873a8611313e4afb3ab77e09f76d99a2
fdrbrs <federico.borsci@gmail.com>
2021-08-02 23:58:58 +0200

    sort posts in desc order in Index

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -15,7 +15,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::all();
+        $posts = Post::all()->sortByDesc('id');
         return view('admin.posts.index', compact('posts'));
     }
 
